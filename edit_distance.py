acf0223b967a1b041ad40cf669f8ca812a9b370f
gy910210 <gy910210@163.com>
2015-04-25 16:54:00 +0800

    fix edit_distance

--- edit_distance.py
+++ edit_distance.py
@@ -10,42 +10,42 @@ def edit(string1, string2):
 
     for ii in xrange(1, n + 1):
         record[0][ii] = ii
-        pos_record[0][ii] = (-1, -1, '#', string2[ii - 1])
+        pos_record[0][ii] = (-1, ii - 1, '#', string2[ii - 1])
 
     for jj in xrange(1, m + 1):
         record[jj][0] = jj
-        pos_record[jj][0] = (-1, -1, string1[jj - 1], '#')
+        pos_record[jj][0] = (jj - 1, -1, string1[jj - 1], '#')
 
     for i in xrange(1, m + 1):
         for j in xrange(1, n + 1):
             tmp = 0
             if string1[i - 1] != string2[j - 1]:
                 tmp = 1
-            record[i][j] = min(min(record[i - 1][j] + 1, record[i][j - 1] + 1), record[i - 1][j - 1] + tmp)
 
             pos = ()
             if record[i - 1][j] + 1 < record[i][j - 1] + 1:
                 if record[i - 1][j] + 1 < record[i - 1][j - 1] + tmp:
-                    pos = (i - 1, j, string2[j - 1], '#')
+                    pos = (i - 1, j, string1[i - 1], '#')
                 else:
                     pos = (i - 1, j - 1, string1[i - 1], string2[j - 1])
             else:
                 if record[i][j - 1] + 1 < record[i - 1][j - 1] + tmp:
-                    pos = (i, j - 1, '#', string1[i - 1])
+                    pos = (i, j - 1, '#', string2[j - 1])
                 else:
                     pos = (i - 1, j - 1, string1[i - 1], string2[j - 1])
             pos_record[i][j] = pos
+            record[i][j] = min(record[i - 1][j] + 1, record[i][j - 1] + 1, record[i - 1][j - 1] + tmp)
 
     pos_vector = []
     i, j = m, n
-    while i >= 0 and j >= 0:
+    while i > 0 or j > 0:
         i, j, ch1, ch2 = pos_record[i][j]
         pos_vector.append((ch1, ch2))
     pos_vector.reverse()
 
     return record[m][n], pos_vector
 
-
+'''
 def edit_dl(string1, string2):
     m = len(string1)
     n = len(string2)
@@ -67,6 +67,7 @@ def edit_dl(string1, string2):
                     tmp = 1
             record[i][j] = min(min(record[i - 1][j] + 1, record[i][j - 1] + 1), record[i - 1][j - 1] + tmp)
     return record[m][n]
+'''
 
 if __name__ == '__main__':
-    print edit('physical', 'fisikle')
+    print edit('abb', 'ba')
